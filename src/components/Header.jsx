b68bf06be7856cd60ffcfc0f69b0d762daa2a6aa
Eliot Johnson <eliotmjohnson@gmail.com>
2023-06-02 14:18:49 -0500

    Changes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import classes from "./Header.module.css";
+import foodIcon from "../assets/favicon.png"
 import { Link } from "react-router-dom";
 
 const Header = () => {
 	return (
 		<header className={classes.header}>
-			<h2 className={classes.title}>Devmountain Eatery</h2>
+			<h2 className={classes.title}>
+				Devmountain Eatery
+				<img src={foodIcon} />
+			</h2>
 			<nav className={classes.nav}>
 				<Link to="">
 					<button>Home</button>
